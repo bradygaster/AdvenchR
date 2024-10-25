debcfee96c1077949624f40334acf7cb36f4e75c
Brady Gaster <bradyg@microsoft.com>
2024-10-24 23:16:44 -0700

    upgraded to aspire 9

--- AdvenchR.AppHost/Program.cs
+++ AdvenchR.AppHost/Program.cs
@@ -1,5 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.AdvenchR>("advenchr");
+builder.AddProject<Projects.AdvenchR>("frontend");
 
 builder.Build().Run();
